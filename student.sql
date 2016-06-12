f2b0a5f2ff1d4a23c173b78b5bb0a59888921c4f
yunting <yunting@2Dan.xiyoulinux>
2016-06-12 08:36:48 +0800

    sql创建表脚本

---
+++ student.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE Info
+(
+	stu_id int NOT NULL AUTO_INCREMENT,
+	stu_name char(30) NOT NULL,
+	stu_age int NOT NULL,
+	grade_cla int NULL,
+	grade_math int NULL,
+	score int NULL,
+	stu_mail char(30) NULL,
+	PRIMARY KEY (stu_id)
+);
